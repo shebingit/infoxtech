e67a87bca5abf844ad589731960be66518e44f69
Your Name <you@example.com>
2022-07-02 14:27:39 +0530

    commit2/7

--- infoxsite/settings.py
+++ infoxsite/settings.py
@@ -76,16 +76,14 @@ WSGI_APPLICATION = 'infoxsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'karatte',
-        'USER':'root',
-        'PASSWORD':'',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
